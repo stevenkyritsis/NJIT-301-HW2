9337c7713d1b67bf89933c4b3e39c5eaabceb1f2
Steven <16929888+stevenkyritsis@users.noreply.github.com>
2021-10-04 11:05:46 -0400

    Oui

--- CS301_HW2.py
+++ CS301_HW2.py
@@ -5,28 +5,29 @@ from matplotlib import pyplot as plt
 # poisson distribution data for y-axis
 s = np.random.poisson(lam=10, size=100)
 
-plt.hist(s)
+y = plt.hist(s)
 
 # showing the graph
 plt.show()
 
 #2
-def gradient_descent(x, y, theta, alpha, m, numIterations):
-    xTrans = x.transpose()
-    for i in range(0, numIterations):
-        hypothesis = np.dot(x, theta)
-        loss = hypothesis - y
-        # avg cost per example (the 2 in 2*m doesn't really matter here.
-        # But to be consistent with the gradient, I include it)
-        cost = np.sum(loss ** 2) / (2 * m)
-        print("Iteration %d | Cost: %f" % (i, cost))
-        # avg gradient per example
-        gradient = np.dot(xTrans, loss) / m
-        # update
-        theta = theta - alpha * gradient
-    return theta
+def GD(f, start, lr, n_iter=50, tol=1e-05):
+    res = start
+      
+    for _ in range(n_iter): 
+        # graident is calculated using the np.gradient 
+        # function.
+        new_val = -lr * np.gradient(f)
+        if np.all(np.abs(new_val) <= tol):
+            break
+        res += new_val
+          
+    # we return a vector as the gradient can be
+    # multivariable function. if the function has 1
+    # dependent variable then it returns a scalar value.
+    return res
 
 mu = np.mean(s)
 var = np.var(s, ddof=1)
 
-gradient_descent(gradient=10.0, start=100.0, learn_rate=0.2, n_iter=1)
+GD(s, y, 10.0, 100.0, 0.2, 1)
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # NJIT-301-HW2
+<h3> By Steven Kyritsis </h3>
+
+First I import numpy as np and pyplot as plt from matplot.
+
+
+```python
+import numpy as np
+from matplotlib import pyplot as plt
+```
+
+For the first problem I made a variable "s" to hold the values of the poisson distribution using the following below:
+```python
+s = np.random.poisson(lam=10, size=100)
+```
+
+Next I created a histogram array thad stored the values in the variable "y"(we will need the values for the next problem)
+
+```python
+y = plt.hist(s)
+```
+
+To plot the graph I used the following:
+```python
+plt.show()
+```
+
+<p>This is the output of the graph:<br></p>
+
+![Graph]
+(image1.png)
+
