5c201cf0d042d16e20bc3174be1e1020713802f4
Steven <16929888+stevenkyritsis@users.noreply.github.com>
2021-10-04 13:37:24 -0400

    L

--- CS301_HW2.py
+++ CS301_HW2.py
@@ -5,7 +5,10 @@ from matplotlib import pyplot as plt
 # poisson distribution data for y-axis
 s = np.random.poisson(lam=10, size=100)
 
-y = plt.hist(s)
+y = plt.hist(s, 10, density=True)
+
+plt.xlabel('Time')
+plt.ylabel('Visitors')
 
 # showing the graph
 plt.show()
@@ -13,21 +16,14 @@ plt.show()
 #2
 def GD(f, start, lr, n_iter=50, tol=1e-05):
     result = start
-      
     for _ in range(n_iter): 
-        # graident is calculated using the np.gradient 
-        # function.
         new_val = -lr * np.gradient(f)
         if np.all(np.abs(new_val) <= tol):
             break
         result += new_val
-          
-    # we return a vector as the gradient can be
-    # multivariable function. if the function has 1
-    # dependent variable then it returns a scalar value.
     return result
 
 mu = np.mean(s)
 var = np.var(s, ddof=1)
 
-GD(s, y, 10.0, 100.0, 0.2, 1)
+GD(s, y, 0.2)
